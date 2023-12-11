58d3d64625ef7b5e79b3879d16963aa0968628cc
David Andrea <davejandrea@gmail.com>
2023-12-10 23:31:15 -0400

    ready to add GraphQL

---
+++ init.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS invoices_dev (
+    id varchar not null primary key,
+    created_at varchar not null,
+    payment_due varchar,
+    description varchar,
+    payment_terms integer,
+    client_name varchar,
+    client_email varchar,
+    status varchar,
+    sender_street varchar,
+    sender_city varchar,
+    sender_postcode varchar,
+    sender_country varchar,
+    client_street varchar,
+    client_city varchar,
+    client_postcode varchar,
+    client_country varchar,
+
+)
