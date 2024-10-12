9737b220cca31aebc79f0237830f39e9d4db6f54
David Andrea <davejandrea@gmail.com>
2024-10-12 19:29:53 -0300

    refactor index.ts - split into app and server

--- invoice-backend/src/index.ts
+++ invoice-backend/src/index.ts
@@ -1,102 +1,14 @@
-import "reflect-metadata";
-import http from "http";
-import typeDefs from "./GraphQL/typeDefs";
-import { WebSocketServer } from "ws";
-import { useServer } from "graphql-ws/lib/use/ws";
-import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
-import { makeExecutableSchema } from "@graphql-tools/schema";
-import { ApolloServer } from "@apollo/server";
-import { ContextArgs, MyContext } from "./constants/types";
-import {
-  ApolloServerPluginLandingPageLocalDefault,
-  ApolloServerPluginLandingPageProductionDefault,
-} from "@apollo/server/plugin/landingPage/default";
-import { InversifyExpressServer } from "inversify-express-utils";
-import container from "./config/inversify.config";
-import {
-  NODE_ENV,
-  PORT,
-  serverConfig,
-  serverErrorConfig,
-} from "./config/server.config";
+// index.ts
+import { PORT } from "./config/server.config";
+import { createServer } from "./server";
 
-import { expressMiddleware } from "@apollo/server/express4";
-import { createContext } from "./GraphQL/createContext";
-import "./controllers/invoice.controller";
-import InvoiceController from "./controllers/invoice.controller";
-import { Request, Response } from "express";
-import { DatabaseConnection } from "./database/prisma.database.connection";
-// process.env.NODE_ENV = "production";
-
-const start = async () => {
-  try {
-    const inversifyServer = new InversifyExpressServer(container);
-    inversifyServer.setConfig(serverConfig);
-    inversifyServer.setErrorConfig(serverErrorConfig);
-
-    const database = container.get(DatabaseConnection);
-    await database.initConnection();
-
-    const app = inversifyServer.build();
-
-    const httpServer = http.createServer(app);
-    const wsServer = new WebSocketServer({
-      server: httpServer,
-      path: "/",
-    });
-
-    const controller = container.get(InvoiceController);
-    const resolvers = controller.resolvers;
-    const schema = makeExecutableSchema({ typeDefs, resolvers });
-    const serverCleanup = useServer({ schema }, wsServer);
-
-    const server = new ApolloServer<MyContext>({
-      schema,
-      introspection: true,
-      status400ForVariableCoercionErrors: true,
-      plugins: [
-        ApolloServerPluginDrainHttpServer({ httpServer }),
-        {
-          async serverWillStart() {
-            return {
-              async drainServer() {
-                await serverCleanup.dispose();
-              },
-            };
-          },
-        },
-
-        // Install a landing page plugin based on NODE_ENV
-        NODE_ENV === "production"
-          ? ApolloServerPluginLandingPageProductionDefault({
-              graphRef: "my-graph-id@my-graph-variant",
-              footer: false,
-            })
-          : ApolloServerPluginLandingPageLocalDefault({ footer: false }),
-      ],
-    });
-
-    await server.start();
-
-    app.use(
-      expressMiddleware(server, {
-        context: async ({ req, connection }: ContextArgs) =>
-          await createContext({ req, connection }),
-      }),
-    );
-
-    app.get("/api/ping", (_req: Request, res: Response) => {
-      console.log("someone pinged here");
-      res.send("pong");
-    });
-
+createServer()
+  .then(([_, httpServer]) => {
     httpServer.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
-  } catch (error) {
-    console.log(error);
-    console.log("Server error:");
-  }
-};
-
-start();
+  })
+  .catch((error) => {
+    console.error("Error starting server:", error);
+    process.exit(1);
+  });
---
+++ invoice-backend/src/server.ts
@@ -0,0 +1,73 @@
+import http from "http";
+import { WebSocketServer } from "ws";
+import { useServer } from "graphql-ws/lib/use/ws";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { ApolloServer } from "@apollo/server";
+import { ContextArgs, MyContext } from "./constants/types";
+import {
+  ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageProductionDefault,
+} from "@apollo/server/plugin/landingPage/default";
+import { createApp } from "./app";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import typeDefs from "./GraphQL/typeDefs";
+import InvoiceController from "./controllers/invoice.controller";
+import container from "./config/inversify.config";
+import { NODE_ENV } from "./config/server.config";
+import { expressMiddleware } from "@apollo/server/express4";
+import { createContext } from "./GraphQL/createContext";
+
+export const createServer = async () => {
+  try {
+    const app = await createApp();
+    const httpServer = http.createServer(app);
+    const wsServer = new WebSocketServer({
+      server: httpServer,
+      path: "/",
+    });
+
+    const controller = container.get(InvoiceController);
+    const resolvers = controller.resolvers;
+    const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+    const serverCleanup = useServer({ schema }, wsServer);
+
+    const server = new ApolloServer<MyContext>({
+      schema,
+      introspection: true,
+      status400ForVariableCoercionErrors: true,
+      plugins: [
+        ApolloServerPluginDrainHttpServer({ httpServer }),
+        {
+          async serverWillStart() {
+            return {
+              async drainServer() {
+                await serverCleanup.dispose();
+              },
+            };
+          },
+        },
+        NODE_ENV === "production"
+          ? ApolloServerPluginLandingPageProductionDefault({
+              graphRef: "my-graph-id@my-graph-variant",
+              footer: false,
+            })
+          : ApolloServerPluginLandingPageLocalDefault({ footer: false }),
+      ],
+    });
+
+    await server.start();
+
+    app.use(
+      expressMiddleware(server, {
+        context: async ({ req, connection }: ContextArgs) =>
+          await createContext({ req, connection }),
+      }),
+    );
+
+    return [app, httpServer];
+  } catch (error) {
+    console.error("Server startup error:", error);
+    process.exit(1);
+  }
+};
