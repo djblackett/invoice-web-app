15c873c04b958e20506a2a5536671e5085295f42
David Andrea <davejandrea@gmail.com>
2024-10-26 19:24:18 -0300

    create custom hook for fetching one invoice by id

--- invoice-frontend/src/pages/ViewInvoice.tsx
+++ invoice-frontend/src/pages/ViewInvoice.tsx
@@ -1,28 +1,23 @@
 import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { useApolloClient, useQuery } from "@apollo/client";
 import InvoiceToolbar from "../components/invoice-components/InvoiceToolbar";
 import FullInvoice from "../components/invoice-components/FullInvoice";
 import EditForm from "./EditForm";
 import DeleteModal from "../components/DeleteModal";
 import { arrowLeft, GoBack, GoBackButton, Icon, ViewContainer } from "../styles/ViewInvoiceStyles";
-import { GET_INVOICE_BY_ID } from "../graphql/queries";
+import useInvoice from "../hooks/useInvoice";
+import { NewInvoiceProvider } from "../components/form-components/NewInvoiceContextProvider";
 
 function ViewInvoice() {
   const { id } = useParams();
-  console.log(id);
+
 
   const navigate = useNavigate();
-  const apolloClient = useApolloClient();
   const [isEditOpen, setIsEditOpen] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const invoice = useQuery(GET_INVOICE_BY_ID, {
-    client: apolloClient,
-    variables: { getInvoiceByIdId: id }
-  });
+  const { invoice, loading, error } = useInvoice();
 
-  console.log(invoice.data);
   const toggleEditTab = () => {
     setIsEditOpen(!isEditOpen);
   };
@@ -31,36 +26,39 @@ function ViewInvoice() {
     navigate("/");
   };
 
-  if (invoice.loading) {
+  if (loading) {
     return <h2>Loading</h2>;
   }
 
-  if (invoice.error) return <p>Error: {invoice.error.message}</p>;
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
 
   return (
     <ViewContainer>
-      <EditForm
-        isEditOpen={isEditOpen}
-        setIsEditOpen={setIsEditOpen}
-        id={id as string}
-        invoice={invoice.data.getInvoiceById}
-      />
-      <GoBackButton onClick={goBack}>
-        <Icon>{arrowLeft}</Icon>
-        <GoBack>Go back</GoBack>
-      </GoBackButton>
-      <InvoiceToolbar
-        invoice={invoice?.data.getInvoiceById}
-        setEdit={toggleEditTab}
-        setIsModalOpen={setIsModalOpen}
-        isEditOpen={isEditOpen}
-      />
-      <FullInvoice invoice={invoice.data.getInvoiceById} />
-      <DeleteModal
-        setIsModalOpen={setIsModalOpen}
-        isModalOpen={isModalOpen}
-        id={id as string}
-      />
+      <NewInvoiceProvider>
+        <EditForm
+          isEditOpen={isEditOpen}
+          setIsEditOpen={setIsEditOpen}
+          id={id as string}
+          invoice={invoice}
+        />
+        <GoBackButton onClick={goBack}>
+          <Icon>{arrowLeft}</Icon>
+          <GoBack>Go back</GoBack>
+        </GoBackButton>
+        <InvoiceToolbar
+          invoice={invoice}
+          setEdit={toggleEditTab}
+          setIsModalOpen={setIsModalOpen}
+          isEditOpen={isEditOpen}
+        />
+        <FullInvoice invoice={invoice} />
+        <DeleteModal
+          setIsModalOpen={setIsModalOpen}
+          isModalOpen={isModalOpen}
+        />
+      </NewInvoiceProvider>
     </ViewContainer>
   );
 }
