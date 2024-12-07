3196e1ce0176368ac79ad81dd840ad59a21e6f49
David Andrea <davejandrea@gmail.com>
2024-12-06 20:47:57 -0400

    remove global types file

--- invoice-frontend/global.t.ts
+++
@@ -1,9 +0,0 @@
-import type { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers";
-import { expect } from "vitest";
-
-declare global {
-  namespace jest {
-    interface Matchers<R = void>
-      extends TestingLibraryMatchers<typeof expect.stringContaining, R> {}
-  }
-}
