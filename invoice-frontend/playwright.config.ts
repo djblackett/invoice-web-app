512cc7921c93998400e9fc668747b8e4a112e384
David Andrea <davejandrea@gmail.com>
2024-12-09 16:11:46 -0400

    try built-in server runner for playwright

--- invoice-frontend/playwright.config.ts
+++ invoice-frontend/playwright.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   testMatch: "*.spec.ts",
   timeout: 60000,
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  // fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -27,6 +27,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "http://127.0.0.1:5173",
+    video: "off", // Disable video recording
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -71,9 +72,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "npm run dev",
+    url: "http://127.0.0.1:5173",
+    reuseExistingServer: !process.env.CI,
+  },
 });
