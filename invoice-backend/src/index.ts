b6d6ebdc6bb356ac8397686e294633560394e0c0
David Andrea <davejandrea@gmail.com>
2024-12-13 00:42:17 -0400

    debug

--- invoice-backend/src/index.ts
+++ invoice-backend/src/index.ts
@@ -1,11 +1,17 @@
-import { PORT } from "./config/server.config";
+import { NODE_ENV, PORT } from "./config/server.config";
 import { createServer } from "./server";
 
 createServer()
   .then(([_, httpServer]) => {
-    httpServer.listen(PORT, "0.0.0.0", () => {
-      console.log(`Server running on port ${PORT}`);
-    });
+    if (NODE_ENV === "production") {
+      httpServer.listen(PORT, "0.0.0.0", () => {
+        console.log(`Server running on  0.0.0.0:${PORT}`);
+      });
+    } else {
+      httpServer.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+      });
+    }
   })
   .catch((error) => {
     console.error("Error starting server:", error);
