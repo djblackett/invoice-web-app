8f994617b173bb324ea1f60bc2b6e836f99d76a8
David Andrea <davejandrea@gmail.com>
2024-12-14 17:12:12 -0400

    fix frontend stye/lint rules

--- invoice-frontend/prettier.config.js
+++ invoice-frontend/prettier.config.js
@@ -1,19 +1,11 @@
-export const arrowParens = "always";
-export const semi = true;
-export const trailingComma = "all";
-export const singleQuote = false;
-export const endOfLine = "lf";
-export const indent = "2";
-export const plugins = [];
-
 const config = {
   semi: true,
   singleQuote: false,
   endOfLine: "lf",
-  indent: "2",
+  tabWidth: 2,
   trailingComma: "all",
   plugins: [
-    "eslint-prettier",
+    "prettier-eslint",
     "eslint-plugin-prettier",
     "eslint-config-prettier",
   ],
