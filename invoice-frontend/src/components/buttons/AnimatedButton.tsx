e650b9f442632dacbe93cd8a8e011fb0b9fe8679
David Andrea <davejandrea@gmail.com>
2024-12-20 18:00:58 -0400

    trigger frontend deploy

--- invoice-frontend/src/components/buttons/AnimatedButton.tsx
+++ invoice-frontend/src/components/buttons/AnimatedButton.tsx
@@ -1,22 +1,6 @@
 import { motion } from "framer-motion";
 import styled from "styled-components";
 
-// .App {
-//   font-family: sans-serif;
-//   text-align: center;
-//   padding: 100px 30px;
-// }
-
-// button {
-//   font-size: 1.25rem;
-//   background-color: #0f75ae;
-//   padding: 1rem 1.5rem;
-//   border-radius: 1rem;
-//   cursor: pointer;
-//   border: none;
-//   color: #fafbfc;
-// }
-
 export const SlidingComponent = styled(motion.div)`
   justify-self: center;
   align-self: center;
