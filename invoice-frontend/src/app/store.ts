79c733c4df56d995508f55b4097926ee88740ceb
David <davejandrea@gmail.com>
2025-01-05 13:16:33 -0400

    refactor

--- invoice-frontend/src/app/store.ts
+++ invoice-frontend/src/app/store.ts
@@ -1,5 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import invoicesReducer from "../features/invoices/invoicesSlice";
 import filterReducer from "../features/invoices/filterSlice";
 
 const store = configureStore({
