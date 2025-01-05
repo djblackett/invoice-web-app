7316bb56577bbbc3d8e06c5e2ccc2317e49b0e96
David <davejandrea@gmail.com>
2025-01-05 13:10:16 -0400

    add axe-core accessibility test

--- invoice-frontend/qa/pages/invoices/invoice-main-page.ts
+++ invoice-frontend/qa/pages/invoices/invoice-main-page.ts
@@ -1,7 +1,8 @@
-import { Locator, Page } from "@playwright/test";
+import { expect, Locator, Page } from "@playwright/test";
 import BasePage from "../base/base-page";
 import { WelcomePage } from "./welcome-page";
 import { Header } from "./header";
+import AxeBuilder from "@axe-core/playwright";
 
 export class InvoiceMainPage extends BasePage {
   readonly welcomePage: WelcomePage;
@@ -19,5 +20,35 @@ export class InvoiceMainPage extends BasePage {
         ? "https://localhost:4173/invoice-web-app/"
         : "https://localhost:5173/invoice-web-app/",
     );
+  }
+
+  public async runAccessibilityTest(): Promise<void> {
+    const accessibilityScanResults = await new AxeBuilder({
+      page: this.page,
+    })
+      // Exclude the rule that checks for a missing <h1> tag
+      // .disableRules("page-has-heading-one")
+      .analyze();
+
+    if (accessibilityScanResults.violations.length > 0) {
+      const formattedViolations = accessibilityScanResults.violations
+        .map((violation, index) => {
+          const nodes = violation.nodes
+            .map((node) => node.target.join(", "))
+            .join(", ");
+          return `
+          ${index + 1}. **${violation.id}** (${violation.impact})
+             - **Description:** ${violation.description}
+             - **Help:** ${violation.help} (${violation.helpUrl})
+             - **Affected Elements:** ${nodes}
+          `;
+        })
+        .join("\n");
+
+      throw new Error(
+        `Accessibility violations found:\n${formattedViolations}`,
+      );
+      // expect(accessibilityScanResults.violations).toEqual([]);
+    }
   }
 }
---
+++ invoice-frontend/qa/tests/accessibility/accessibility-axe.spec.ts
@@ -0,0 +1,16 @@
+import { test } from "../../fixtures/base";
+
+test.describe("Accessibility tests for the text editor", () => {
+  test("should have no detectable accessibility issues on load", async ({
+    invoiceMainPage,
+  }) => {
+    await invoiceMainPage.runAccessibilityTest();
+  });
+
+  //   test.skip("should have no detectable accessibility issues after making text bold", async ({
+  //     invoiceMainPage,
+  //   }) => {
+  //     // await invoiceMainPage.toolbar.boldButton.click();
+  //     // await invoiceMainPage.runAccessibilityTest();
+  //   });
+});
