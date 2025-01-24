3e4810a993d0325eec034c7c39184f449ba8ff37
David <davejandrea@gmail.com>
2025-01-24 00:35:44 -0400

    add animation to each invoice card instead of parent container

--- invoice-frontend/src/components/invoice-components/InvoiceCard.tsx
+++ invoice-frontend/src/components/invoice-components/InvoiceCard.tsx
@@ -152,7 +152,16 @@ function InvoiceCard({ invoice }: InvoiceCardProps) {
   };
 
   return (
-    <Card whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
+    <Card
+      whileHover={{ scale: 1.05 }}
+      whileTap={{ scale: 0.95 }}
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.4,
+        scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
+      }}
+    >
       <IDNumber>
         <span style={{ color: "#7E88C3" }}>#</span>
         {invoice.id.substring(0, 6)}
