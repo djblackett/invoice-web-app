f894274791b1caa34dc9f716fb01e14d50644de4
David <davejandrea@gmail.com>
2025-02-04 12:46:54 -0400

    remove migrations

--- invoice-backend/prisma/migrations/20250127172423_add_default_created_by/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Invoice" ALTER COLUMN "createdById" SET DEFAULT 1;
--- invoice-backend/prisma/migrations/20250203030628_cascade/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Invoice" DROP CONSTRAINT "Invoice_createdById_fkey";
-
--- AddForeignKey
-ALTER TABLE "Invoice" ADD CONSTRAINT "Invoice_createdById_fkey" FOREIGN KEY ("createdById") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
