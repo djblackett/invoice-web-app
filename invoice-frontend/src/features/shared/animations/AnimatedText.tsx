2392de4b80d1cb9a8da4d354405500588b8c5aca
David <davejandrea@gmail.com>
2025-03-05 14:38:03 -0400

    fix welcome text top margin on mobile

--- invoice-frontend/src/features/shared/animations/AnimatedText.tsx
+++ invoice-frontend/src/features/shared/animations/AnimatedText.tsx
@@ -4,14 +4,19 @@ import styled from "styled-components";
 const AnimatedText = styled.h1`
   position: relative;
   font-family: ${({ theme }) => theme.font};
-  font-size: 64px;
+  font-size: 3rem;
   font-weight: 900;
   letter-spacing: -1px;
   line-height: 1.2;
   text-align: center;
-  margin-top: 20%;
+  margin-top: 40%;
   margin-left: 0;
   color: ${({ theme }) => theme.text};
+
+  @media (min-width: 768px) {
+    margin-top: 20%;
+    font-size: 4rem;
+  }
 `;
 
 type TextAnimationProps = {
