76522536df23a43effe602b7c4ebf0726136527c
David <davejandrea@gmail.com>
2025-04-23 20:49:32 -0300

    update backend type and lint config

--- invoice-backend/eslint.config.mjs
+++ invoice-backend/eslint.config.mjs
@@ -2,6 +2,7 @@ import eslint from "@eslint/js";
 import { globalIgnores } from "eslint/config";
 import tseslint from "typescript-eslint";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import pluginSecurity from "eslint-plugin-security";
 
 // /** @type {import('eslint').Linter.Config[]} */
 export default tseslint.config(
@@ -26,9 +27,20 @@ export default tseslint.config(
   ...tseslint.configs.recommendedTypeChecked,
   tseslint.configs.strict,
   eslintPluginPrettierRecommended,
+  pluginSecurity.configs.recommended,
   {
     rules: {
-      // "@typescript-eslint/no-unnecessary-condition": "error",
+      "@typescript-eslint/no-unnecessary-condition": "error",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "error",
+      // Promises
+      "@typescript-eslint/no-floating-promises": "error", // un-awaited promises :contentReference[oaicite:3]{index=3}
+      "@typescript-eslint/no-misused-promises": "error", // passing promises where a bool/callback was meant :contentReference[oaicite:4]{index=4}
+      "@typescript-eslint/no-unsafe-assignment": "error",
+      "@typescript-eslint/no-unsafe-member-access": "error",
+      "@typescript-eslint/no-unsafe-return": "error",
+      // Prefer type-only imports for clarity
+      "@typescript-eslint/consistent-type-imports": "error",
     },
   },
 );
